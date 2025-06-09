68c66b997a506aa61c581e00959418d47bc0699f
Carlos Calel <ccalelc2@gmail.com>
2025-06-09 01:40:53 -0600

    javascript añadido al slider

--- build/js/app.js
+++ build/js/app.js
@@ -5,3 +5,9 @@ menuToggle.addEventListener('click', function () {
     menuToggle.classList.toggle('header__toggle--activo');
     navMenu.classList.toggle('navegacion__menu--activo');
 });
+
+let logosTech = document.getElementById('slider-tech').cloneNode(true);
+document.getElementById('logos-tech').appendChild(logosTech);
+
+let logosTools = document.getElementById('slider-tools').cloneNode(true);
+document.getElementById('logos-tools').appendChild(logosTools);
